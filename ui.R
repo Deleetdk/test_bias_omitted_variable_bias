cd2d84fe083954fc9bbccdf3f1400365f1912539
Emil O. W. Kirkegaard <emil@emilkirkegaard.dk>
2015-06-22 21:26:26 +0200

    change title

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ library(shiny)
 shinyUI(fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Test bias and ommitted variable bias"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
